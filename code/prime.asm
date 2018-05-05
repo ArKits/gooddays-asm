ace44768678e5b2236a6f36999d6a1d87f94024c
Archit  Khode <ArKits@users.noreply.github.com>
2018-05-04 22:51:22 -0700

    Added prettyprint

--- code/prime.asm
+++ code/prime.asm
@@ -9,43 +9,45 @@ INCLUDE Irvine32.inc
 ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-prime_array DWORD 1000 DUP(0)					; Store primes
-NumOfFactors DWORD 0						; Store number of factors
-checkMe DWORD 0							; Store the number to check	
+prime_array DWORD 1000 DUP(0)				; Store primes
+NumOfFactors DWORD 0					; Store number of factors
+checkMe DWORD 0						; Store the number to check	
+prettyprint DWORD " ,"					
 
 .code
 
 main PROC
 
-mov ecx, 1000							; Count till 1000 number	
-mov eax, 1							; Start from 1
-mov esi, OFFSET prime_array					; Offset array
+mov ecx, 1000						; Count till 1000 number	
+mov eax, 1						; Start from 1
+mov esi, OFFSET prime_array				; Offset array
 
 L1:
-mov checkMe, eax						; Set the number to check
-push ecx							; Save counter
+mov checkMe, eax					; Set the number to check
+push ecx						; Save counter
 
-	mov ecx, checkMe					; Set counter to checking number
-	mov NumOfFactors, 0					; Reset NumOfFactors
+	mov ecx, checkMe				; Set counter to checking number
+	mov NumOfFactors, 0				; Reset NumOfFactors
 
 	L2:
 	xor edx, edx
 	mov eax, checkMe
-	div ecx							; Divide checkMe by counter
+	div ecx						; Divide checkMe by counter
 
-	.if edx == 0						; If no remainder, increment NumOfFactors
+	.if edx == 0					; If no remainder, increment NumOfFactors
 	inc NumOfFactors
 	.endif
 
 	loop L2
 
-	pop ecx							; Revert to original loop counter
+	pop ecx						; Revert to original loop counter
 
 	.if NumOfFactors == 2	
-	mov edx, OFFSET checkMe					; Print to Console
+	mov edx, OFFSET checkMe				; Print to Console
 	call WriteInt
-	call Crlf
-	mov eax, checkMe					; Add to array
+	mov edx, OFFSET prettyprint			; Print it pretty
+	call WriteString
+	mov eax, checkMe				; Add to array
 	mov [esi + ecx * 4], eax 
 	.endif
 
