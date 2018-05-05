33def260f31f450701508c728c8329b658de7df7
Archit  Khode <ArKits@users.noreply.github.com>
2018-05-04 22:42:35 -0700

    Printing to console and fixed comments

--- code/prime.asm
+++ code/prime.asm
@@ -9,8 +9,8 @@ INCLUDE Irvine32.inc
 ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-prime_array DWORD 1000 DUP(0)			; Store primes
-NumOfFactors DWORD 0					; Store number of factors
+prime_array DWORD 1000 DUP(0)					; Store primes
+NumOfFactors DWORD 0						; Store number of factors
 checkMe DWORD 0							; Store the number to check	
 
 .code
@@ -18,12 +18,12 @@ checkMe DWORD 0							; Store the number to check
 main PROC
 
 mov ecx, 1000							; Count till 1000 number	
-mov eax, 1							; Start from 5
-mov esi, OFFSET prime_array				; Offset array
+mov eax, 1							; Start from 1
+mov esi, OFFSET prime_array					; Offset array
 
 L1:
 mov checkMe, eax						; Set the number to check
-push ecx								; Save counter
+push ecx							; Save counter
 
 	mov ecx, checkMe					; Set counter to checking number
 	mov NumOfFactors, 0					; Reset NumOfFactors
@@ -31,7 +31,7 @@ push ecx								; Save counter
 	L2:
 	xor edx, edx
 	mov eax, checkMe
-	div ecx								; Divide checkMe by counter
+	div ecx							; Divide checkMe by counter
 
 	.if edx == 0						; If no remainder, increment NumOfFactors
 	inc NumOfFactors
@@ -39,9 +39,12 @@ push ecx								; Save counter
 
 	loop L2
 
-	pop ecx								; Revert to original loop counter
+	pop ecx							; Revert to original loop counter
 
-	.if NumOfFactors == 2				
+	.if NumOfFactors == 2	
+	mov edx, OFFSET checkMe					; Print to Console
+	call WriteInt
+	call Crlf
 	mov eax, checkMe					; Add to array
 	mov [esi + ecx * 4], eax 
 	.endif
