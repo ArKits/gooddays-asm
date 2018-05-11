274c2eed44a8ba1fdabeac89c634b94757273d63
Archit  Khode <ArKits@users.noreply.github.com>
2018-05-10 17:48:50 -0700

    Added comments

--- code/primeSieveOfEratosthenes.asm
+++ code/primeSieveOfEratosthenes.asm
@@ -15,34 +15,34 @@ prime_array DWORD 1000 DUP(0)			; Store primes
 
 main PROC
 
-mov ecx, 1000							
-mov eax, 1	
+mov ecx, 1000					; Counter till 1000					
+mov eax, 1
 
-init_array:
+init_array:					; Initaliazes array from 1 to 1000
 mov esi, OFFSET prime_array				
 mov [esi + eax * 4], eax 
 inc eax
 loop init_array
 
-mov ecx, 1000	
-mov eax, 2	
+mov ecx, 1000					; Counter till 1000
+mov eax, 2					; Start from 2, because we don't care about 1
 	
 
 L2:
-	push ecx
-	push eax
+	push ecx				; Save counter
+	push eax				; Save eax
 
-	mov ebx, eax
-	mov ecx, 1000							
-	mov eax, 2	
+	mov ebx, eax				; We make the current number the number we want to check against
+	mov ecx, 1000				; Counter till 1000		
+	mov eax, 2				; Start from 2, because we don't care about 1
 
 	L1:
-	push eax
-	push ebx
-	mul ebx
-	cmp eax, 1000
-	jg next
-	mov ebx, 0
+	push eax				; Save eax
+	push ebx				; Save ebx
+	mul ebx					; Multiply eax * ebx
+	cmp eax, 1000				; If multiple is greater than 1000
+	jg next					; Skip it...
+	mov ebx, 0				; Else make number 0
 	mov [esi + eax * 4], ebx
 	next:
 	pop ebx
@@ -59,11 +59,11 @@ mov ecx, 1000
 mov eax, 1	
 mov esi, OFFSET prime_array		
 
-print_prime:
+print_prime:					; Print the array
 	push eax
 	mov ebx, prime_array[eax*4]
 	mov eax, ebx
-	cmp eax, 0
+	cmp eax, 0				; Skip the zeros
 	je skip_print
 	call WriteDec
 	call crlf
